6af68086b0645f1babb469cee4ee50d341da192a
kang lei <131269017+yNotAVAILABLEa@users.noreply.github.com>
2025-03-14 20:53:04 +0800

    Update fp_64.rs

--- rust/algebra/src/fields/models/fp_64.rs
+++ rust/algebra/src/fields/models/fp_64.rs
@@ -302,7 +302,7 @@ impl<P: Fp64Parameters> std::fmt::Debug for Fp64<P> {
 impl<P: Fp64Parameters> Neg for Fp64<P> {
     type Output = Self;
     #[inline]
-    #[must_use]
+    // #[must_use]
     fn neg(self) -> Self {
         if !self.is_zero() {
             let mut tmp = P::MODULUS;
